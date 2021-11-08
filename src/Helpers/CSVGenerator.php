6cde9855f7dc004b1e5b34427e8d35abfe2ac905
mak-1920 <93209149+mak-1920@users.noreply.github.com>
2021-11-08 14:13:48 +0300

    nginx

--- src/Helpers/CSVGenerator.php
+++ src/Helpers/CSVGenerator.php
@@ -19,13 +19,8 @@ class CSVGenerator{
             while(file_exists($shortname.'-'.++$i.'.csv'));
             $shortname .= "-$i";
         }
-        // $ft = fopen($_SERVER['DOCUMENT_ROOT'].'t', 'w');
-        // \fwrite($ft, 'test');
-        // fclose($ft);
-        \var_dump(\file_get_contents($_SERVER['DOCUMENT_ROOT'].'t'));
         $shortname = 'files/'.$shortname.'.csv';
         $fileName = $_SERVER['DOCUMENT_ROOT'].'/'.$shortname;
-        echo $fileName;
 
         $fp = fopen($fileName, 'w');
 
